2d07fca9de3649f461d9aa64818690aadb8e6891
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-09-28 22:53:07 +0300

    async attr has been added to js script

--- source/js/main.js
+++ source/js/main.js
@@ -1,39 +1,18 @@
-var ready = function ( fn ) {
+if(navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
 
-    // Sanity check
-    if ( typeof fn !== 'function' ) return;
+		console.log(latitude);
+		console.log(longitude);
 
-    // If document is already loaded, run method
-    if ( document.readyState === 'complete'  ) {
-        return fn();
-    }
 
-    // Otherwise, wait until document is loaded
-    // The document has finished loading and the document has been parsed but sub-resources such as images, stylesheets and frames are still loading. The state indicates that the DOMContentLoaded event has been fired.
-    document.addEventListener( 'interactive', fn, false );
+	});
+} else {
+	console.log("Geolocation API не поддерживается в вашем браузере");
+}
 
-    // Alternative: The document and all sub-resources have finished loading. The state indicates that the load event has been fired.
-    // document.addEventListener( 'complete', fn, false );
 
-};
-
-// Example
-ready(function() {
-	var latitude = 0,
-		longitude = 0;
-	
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			latitude = position.coords.latitude;
-			longitude = position.coords.longitude;
-		});
-	} else {
-		alert("Geolocation API не поддерживается в вашем браузере");
-	}
-	console.log(latitude);
-	console.log(longitude);
-
-});
 
 // Examples of API calls
 // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
